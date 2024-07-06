c297087a93c51528188a4e8366b94de8bc9200c7
janitor <divij.bhaw15@gmail.com>
2024-07-07 00:54:58 +0800

    fedsaved

---
+++ savedscript.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('redirectButton').addEventListener('click', () => {
+        window.location.href = './index.html';
+    });
+});
