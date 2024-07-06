277011aa003514ef1a8ee2484241227b3529891f
janitor <divij.bhaw15@gmail.com>
2024-07-06 23:55:06 +0800

    fix time

--- script.js
+++ script.js
@@ -1,39 +1,30 @@
-// Get the current time in Perth
-function updatePerthTime() {
-    var perthTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Perth"});
-    document.getElementById("perth-time").innerHTML = "Current Perth Time: " + perthTime;
-}
 
-// Set the date and time we're counting down to (7th of July 2024 at 5:30 PM)
-var countDownDate = new Date("July 7, 2024 17:30:00").getTime();
+        // Set the date and time we're counting down to (7th of July 2024 at 5:30 PM Perth time)
+        var countDownDate = new Date("July 7, 2024 17:30:00 GMT+08:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-    // Update Perth time
-    updatePerthTime();
-    
-    // Get today's date and time
-    var now = new Date().getTime();
+        // Update the count down every 1 second
+        var x = setInterval(function() {
+            // Get today's date and time
+            var now = new Date().getTime();
 
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+            // Find the distance between now and the count down date
+            var distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Output the result in an element with id="timer"
-    document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+            // Output the result in an element with id="timer"
+            document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-    // If the count down is finished, write some text
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("timer").innerHTML = "EXPIRED";
-    }
-}, 1000);
-
+            // If the count down is finished, write some text
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("timer").innerHTML = "TOO LATE";
+            }
+        }, 1000);
 
 let currentIndex = 0;
 
